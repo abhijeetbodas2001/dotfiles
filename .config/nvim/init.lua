db3b95a034b8a2d33a3c1b2cb817848ed8b5f314
Abhijeet Prasad Bodas <55339528+abhijeetbodas2001@users.noreply.github.com>
2024-04-15 14:28:47 +0530

    Update from NDS

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,6 @@
 vim.cmd([[
 let mapleader=" "
-set scrolloff=20
+" set scrolloff=20
 let &t_SI = "\e[6 q"
 let &t_EI = "\e[2 q"
 set wildmenu
@@ -26,51 +26,85 @@ set nocompatible
 set showcmd
 set showmode
 set history=1000
-set mouse=
+set mouse=a
+set nofixeol
+set nofixendofline
 
 call plug#begin('~/.config/nvim/plugins')
-Plug 'https://github.com/nvim-telescope/telescope.nvim'
-Plug 'https://github.com/nvim-lua/plenary.nvim'
-Plug 'https://github.com/lewis6991/gitsigns.nvim'
-Plug 'https://github.com/0x00-ketsu/autosave.nvim'
-Plug 'https://github.com/nvim-treesitter/nvim-treesitter'
-Plug 'https://github.com/neovim/nvim-lspconfig'
-Plug 'https://github.com/navarasu/onedark.nvim'
-Plug 'https://github.com/echasnovski/mini.nvim'
+Plug '~/.config/nvim/plugins/telescope.nvim-0.1.6'
+Plug '~/.config/nvim/plugins/plenary.nvim'
+Plug '~/.config/nvim/plugins/gitsigns.nvim-0.7'
+Plug '~/.config/nvim/plugins/autosave.nvim'
+Plug '~/.config/nvim/plugins/treesitter.nvim'
+Plug '~/.config/nvim/plugins/nvim-lspconfig-master'
+Plug '~/.config/nvim/plugins/onedark.nvim-master'
+Plug '~/.config/nvim/plugins/mini.nvim-0.12.0'
 call plug#end()
 ]])
 
-vim.treesitter.language.add('python')
-vim.treesitter.language.add('bash')
-vim.treesitter.language.add('markdown')
-vim.treesitter.language.add('java')
-require("nvim-treesitter.configs").setup({
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  }
-})
-
+-- Setup for theme
 require('onedark').setup {
   style = 'darker',
   transparent = true,
-  term_colors = false,
   colors = {
     white = "#ffffff",
-    black = "#000000"
+    fg = "#ffffff",
+    black = "#000000",
   },
   highlights = {
     ["@comment"] = {fg = '$green'},
     ["@string"] = {fg = '$orange'},
     ["@variable"] = {fg = '$white'},
+    MiniStatuslineFilename ={fg='$white'}
   },
 }
 vim.cmd([[colorscheme onedark]])
+vim.cmd([[hi vertsplit guifg=fg guibg=bg]])     -- make vsplit line pure white
 
+-- Setup for Tree sitter
+vim.treesitter.language.add('python', { path = "/home/bodaab/.config/nvim/TSInstall/python.so" })
+vim.treesitter.language.add('bash', { path = "/home/bodaab/.config/nvim/TSInstall/bash.so" })
+vim.treesitter.language.add('markdown', { path = "/home/bodaab/.config/nvim/TSInstall/markdown.so" })
+vim.treesitter.language.add('java', { path = "/home/bodaab/.config/nvim/TSInstall/java.so" })
+require("nvim-treesitter.configs").setup({
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+})
+
+-- Setup for LSPs
+vim.lsp.set_log_level("debug")
 require('lspconfig').pyright.setup({
   capabilities = vim.lsp.protocol.make_client_capabilities(),
+  settings = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+    },
+  }
 })
-require('lspconfig').clangd.setup({})
+require('lspconfig').ruff_lsp.setup {
+  on_attach = function(client, bufnr)
+    if client.name == 'ruff_lsp' then
+      -- Disable hover in favor of Pyright
+      client.server_capabilities.hoverProvider = false
+    end
+  end,
+  init_options = {
+    settings = {
+      args = {},
+    }
+  }
+}
+local format_python_with_ruff = function()
+  vim.lsp.buf.format()
+  vim.lsp.buf.code_action({
+    context = { only = { "source.organizeImports" } },
+    apply = true,
+  })
+end
+require('lspconfig').jdtls.setup({})
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
@@ -83,9 +117,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'gD', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '<leader>lR', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<leader>f', format_python_with_ruff, opts)
   end,
 })
 
+-- Setup for Gitsigns plugin
 vim.cmd([[:highlight DiffAdd guifg=#a4cf69]])
 vim.cmd([[:highlight DiffChange guifg=#63c1e6]])
 vim.cmd([[:highlight DiffDelete guifg=#d74f56]])
@@ -104,7 +140,6 @@ require('gitsigns').setup{
       opts.buffer = bufnr
       vim.keymap.set(mode, l, r, opts)
     end
-
     map('n', ']c', function()
       if vim.wo.diff then return ']c' end
       vim.schedule(function() gs.next_hunk() end)
@@ -115,7 +150,6 @@ require('gitsigns').setup{
       vim.schedule(function() gs.prev_hunk() end)
       return '<Ignore>'
     end, {expr=true})
-
     map('n', '<leader>ga', gs.stage_hunk)       -- add
     map('n', '<leader>gr', gs.reset_hunk)       -- reset
     map('n', '<leader>gu', gs.undo_stage_hunk)  -- unstage
@@ -127,10 +161,11 @@ require('gitsigns').setup{
   end
 }
 
+-- Setup for telescope plugin
 require('telescope').setup({
       defaults = {
       layout_config = {
-            horizontal = { width = 0.9, preview_width = 100 },
+            horizontal = { width = 0.9, preview_cutoff = 100 },
       },
     },
 })
@@ -138,61 +173,70 @@ telescope_builtin = require('telescope.builtin')
 vim.keymap.set('n', '<c-p>', telescope_builtin.find_files, {})  -- fuzzy find files
 vim.keymap.set('n', '<c-g>', telescope_builtin.live_grep, {})   -- grep
 vim.keymap.set('n', '<c-f>', telescope_builtin.grep_string, {})   -- grep for word under cursor
+vim.keymap.set('n', '<c-b>', telescope_builtin.buffers, {})   -- buffers
 vim.keymap.set('n', '<leader>gs', telescope_builtin.git_status, {}) -- git status
 vim.keymap.set('n', '<leader>gc', telescope_builtin.git_commits, {})    -- git commits
-vim.keymap.set('n', '<leader>lr', telescope_builtin.lsp_references, {})    -- LSP references
-local partial_func = function(func, opts)
-    return function()
-        func(opts)
-    end
-end
-vim.keymap.set('n', '<leader>bc', partial_func(telescope_builtin.lsp_document_symbols, {symbols = {"function","class","method"}}), {})    -- bread-crumbs
+vim.keymap.set('n', 'gr', telescope_builtin.lsp_references, {})    -- LSP references
+vim.keymap.set('n', 'gb', telescope_builtin.lsp_document_symbols, {})    -- bread-crumbs
 
+-- Autosave files
+require('autosave').setup()
+
+-- Setup for mini plugins
 require('mini.files').setup()
 vim.keymap.set('n', '<leader>e', MiniFiles.open, {})    -- explorer
-
 require('mini.sessions').setup({
     autowrite = true,
 })
 vim.keymap.set('n', '<leader>s', MiniSessions.select, {})
-
-require('autosave').setup()
-
-require('mini.tabline').setup()
 require('mini.comment').setup() -- Comment/uncomment code (gcc, gc in visual)
-require('mini.fuzzy').setup()
-require('mini.completion').setup({
-    lsp_completion = {
-        process_items = MiniFuzzy.process_lsp_items,
-    },
-})
-
-require('mini.pairs').setup() -- Autoinsert closing bracket/quote
+require('mini.completion').setup() -- Auto-complete
 require('mini.cursorword').setup() -- Automatic highlighting of word under cursor
-require('mini.indentscope').setup() -- Show line for current indent level
 require('mini.statusline').setup() -- Status line at bottom
-require('mini.ai').setup() -- better "around" and "in" motions (`a` for function argument, `f` for function call etc)
 require('mini.trailspace').setup() -- highlight trailing whitespace
-require('mini.splitjoin').setup() -- split and join function arguments
-require('mini.starter').setup() -- start screen
 
-local mini_jump = require('mini.jump2d')
-mini_jump.setup({
-    spotter = mini_jump.gen_pattern_spotter('[%w_]+')   -- hints at the start of words (words can include underscore)
+-- Auto-commands
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = "Highlight text which was yanked",
+    group = vim.api.nvim_create_augroup("test", {clear=false}),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
+-- vim.api.nvim_create_autocmd('TermOpen', {
+--     desc = "In terminal buffers, turn off line numbers",
+--     group = vim.api.nvim_create_augroup("test", {clear=false}),
+--     callback = function()
+--         vim.cmd([[
+--         setlocal nonumber norelativenumber
+--         ]])
+--     end,
+-- })
+
 
 -- Key bindings (plugin-independent)
 vim.cmd([[
+" insert current timestamp, start insert on next line
 nnoremap <leader>t :r! date "+\%F \%T"<CR>I#<space><esc>o
-nnoremap <leader>T :r! date "+\%F \%T"<CR>I###<space><esc>o<CR>
+" copy file path of current file
+nnoremap <leader>yf :let @" = expand("%")<CR>
+" clear search highlights
 nnoremap <leader>h :nohlsearch<CR>
-
-nnoremap <leader>w :bd<CR>
-nnoremap <c-j> :bp<CR>
-nnoremap <c-k> :bn<CR>
-tnoremap <c-j> <c-\><c-N>:bp<CR>
-tnoremap <c-k> <c-\><c-N>:bn<CR>
-
+" close window
+nnoremap <leader>w :close<CR>
+" easily jump between window splits
+nnoremap <c-h> <c-w><c-h>
+nnoremap <c-j> <c-w><c-j>
+nnoremap <c-k> <c-w><c-k>
+nnoremap <c-l> <c-w><c-l>
+" move current buffer to a vsplit
+nnoremap <leader>v :vsplit<CR><c-w><c-h><c-o><c-w><c-l>
+" Go to terminal mode
+tnoremap <esc><esc> <c-\><c-N>
+" Enable "copy" mode
+nnoremap <leader>c :set nonumber<CR>:set norelativenumber<CR>:set mouse=<CR>
+" Disable "copy" mode (default)
+nnoremap <leader>C :set number<CR>:set relativenumber<CR>:set mouse=a<CR>
 " Run Python test
 " Working: copies the Unittest class name in register-c, the method name in register-m and the
 " relative path of the test file in register-f. Contructs to pytest command and runs it.
