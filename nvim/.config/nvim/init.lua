98959f08d28e3728fe5596591f0ac3e2950bf9e7
Abhijeet Prasad Bodas <abhijeetbodas2001@gmail.com>
2025-04-24 10:14:06 +0530

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -32,39 +32,34 @@ if executable("rg")
   set grepprg=rg\ --vimgrep\ --smart-case\ --hidden
   set grepformat=%f:%l:%c:%m
 endif
-" Key bindings for quickfix and grep (gt = do grep, and "t"pye out the search term)
-nnoremap <leader>gt :grep<space>
-" yank word under the cursor, search for it
-" always use the "e" register to put things which will be pasted in the exec mode,
-" so that the default register is not polluted
-nnoremap <leader>gw "eyaw:grep<space><c-r>e
-" in visual mode, yank whatever is highlighted and search for it
-vnoremap <leader>gw "ey:grep<space><c-r>e
+" Key bindings for quickfix and grep (cg = command grep)
+nnoremap <leader>cg :grep<space>
+vnoremap <leader>cg "ey:grep<space><c-r>e   " Grep for selected text in visual
+
 " keybinds to move between items in quickfix list
 nnoremap ]q :cnext<CR>
 nnoremap [q :cprev<CR>
-" insert current timestamp, start writing on next line (for taking notes)
+
 " in = Insert (timestamp for) Notes
 nnoremap <leader>in :r! date "+\%F \%T \%a"<CR>I#<space><esc>o
-" prepend timestamp to current line (for writing todos)
 " it = Insert Timestamp
 nnoremap <leader>it I<c-r>=strftime('%Y%m%d %H:%M')<CR><space>\|<space>
+
 " sh = Set (or unset) Highlights off
 nnoremap <leader>sh :nohlsearch<CR>
+
 " cp = Copy, for Pasting outside
 nnoremap <leader>cp gg0"+yG
+
 " jump between window splits
 nnoremap <c-h> <c-w>h
 nnoremap <c-j> <c-w>j
 nnoremap <c-k> <c-w>k
 nnoremap <c-l> <c-w>l
-" go to normal mode when in terminal mode
-tnoremap <esc><esc> <c-\><c-N>
-" enable "copy mode"
-" sc = Settings Copy
+
+" enable "copy mode"  sc = Settings Copy
 nnoremap <leader>sc :set nonumber<CR>:set norelativenumber<CR>:set mouse=<CR>
-" disable "copy mode"
-" sc = Settings Copy (disable)
+" disable "copy mode" sc = Settings Copy (disable)
 nnoremap <leader>sC :set number<CR>:set relativenumber<CR>:set mouse=a<CR>
 
 " Format current file
@@ -74,16 +69,13 @@ autocmd FileType markdown nnoremap <buffer> <leader>f :!mdformat --wrap 80 %<CR>
 " Run / execute current file
 autocmd FileType python nnoremap <buffer> <leader>e :!LOCAL=1 python % <CR>
 
-" Remove trailing whitespace (cs = clean space)
-nnoremap <leader>cs :%s/\s\+$//<CR>
-
-
 call plug#begin()
 Plug 'https://github.com/nvim-telescope/telescope.nvim'
 Plug 'https://github.com/nvim-lua/plenary.nvim'
 Plug 'https://github.com/lewis6991/gitsigns.nvim'
 Plug 'https://github.com/0x00-ketsu/autosave.nvim'
-Plug 'https://github.com/nvim-treesitter/nvim-treesitter'
+Plug 'https://github.com/nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+"Plug 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects'
 Plug 'https://github.com/neovim/nvim-lspconfig'
 Plug 'https://github.com/navarasu/onedark.nvim'
 Plug 'https://github.com/echasnovski/mini.nvim'
@@ -102,24 +94,54 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     end,
 })
 
-vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.opt.foldlevel = 99 -- Start with all folds open
-vim.treesitter.language.add('python')
-vim.treesitter.language.add('bash')
-vim.treesitter.language.add('markdown')
-vim.treesitter.language.add('lua')
-vim.treesitter.language.add('java')
-vim.treesitter.language.add('rust')
-require("nvim-treesitter.configs").setup({
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  fold = {
-      enable = true,
-  }
-})
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = {"markdown", "python", "rust", "lua", "yaml", "toml", "json"},
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    fold = {
+        enable = true,
+    },
+    textobjects = {
+        enable = true,
+    },
+}
+vim.wo.foldmethod = 'expr'
+vim.wo.foldlevel = 99
+vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+-- Enabling this from the above config does not work for some reason
+inc_selection = require('nvim-treesitter.incremental_selection')
+vim.keymap.set("n", "<Enter>", inc_selection.node_incremental)
+vim.keymap.set("v", "<Enter>", inc_selection.node_incremental)
+vim.keymap.set("v", "\\", inc_selection.node_decremental)
+
+ts_utils = require("nvim-treesitter.ts_utils")
+local function fold_siblings()
+    -- FIXME: DOES NOT WORK
+  local ts_utils = require'nvim-treesitter.ts_utils'
+  local current_node = ts_utils.get_node_at_cursor()
+
+  if not current_node then
+    return
+  end
+
+  local parent_node = current_node:parent()
+  if not parent_node then
+    return
+  end
+
+  local children = ts_utils.get_named_children(parent_node)
+  for _, child in ipairs(children) do
+    if child ~= current_node then
+        local start_row, start_col, end_row, end_col = child:range()
+        vim.cmd(string.format("normal! %dG%dGzf", start_row + 1, end_row + 1))
+    end
+  end
+end
+vim.keymap.set("n", "<leader>fc", fold_siblings)
+
 
 
 require('onedark').setup {
@@ -151,7 +173,23 @@ configs.red_knot = {
 
 lspconfig.red_knot.setup {}
 
-lspconfig.rust_analyzer.setup{}
+lspconfig.rust_analyzer.setup({
+    settings = {
+        ["rust-analyzer"] = {
+            numThreads = 2,
+            checkOnSave = false,
+            cachePriming = {
+                numThreads = 2
+            },
+            cargo = {
+                extraEnv = {
+                    RUST_TEST_THREADS = 2,
+                    CARGO_BUILD_JOBS = 2,
+                }
+            }
+        }
+    }
+})
 
 require('lspconfig').pyright.setup({
   capabilities = vim.lsp.protocol.make_client_capabilities(),
@@ -215,8 +253,8 @@ require('telescope').setup({
 })
 telescope_builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>p', telescope_builtin.find_files, {})  -- fuzzy find files
-vim.keymap.set('n', '<leader>g', telescope_builtin.live_grep, {})   -- grep
-vim.keymap.set('n', '<leader>w', telescope_builtin.grep_string, {})   -- grep for word under cursor
+require("multigrep").setup()
+vim.keymap.set('n', '<leader>rw', telescope_builtin.grep_string, {})   -- grep for word under cursor
 vim.keymap.set('n', '<leader>gs', telescope_builtin.git_status, {}) -- git status
 vim.keymap.set('n', '<leader>gc', telescope_builtin.git_commits, {})    -- git commits
 vim.keymap.set('n', '<leader>lr', telescope_builtin.lsp_references, {})    -- LSP references
